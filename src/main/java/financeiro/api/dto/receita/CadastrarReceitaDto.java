a3b4c2f016e25506caa061e115598e3b7757c7d7
Klayton Souza <klayton.al.souza@gmail.com>
2023-11-22 14:58:10 -0300

    0.3.1 - Refatorando requisição POST

--- src/main/java/financeiro/api/dto/receita/CadastrarReceitaDto.java
+++ src/main/java/financeiro/api/dto/receita/CadastrarReceitaDto.java
@@ -1,12 +1,18 @@
 package financeiro.api.dto.receita;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public record CadastrarReceitaDto(
         Long id_receita,
+        @NotNull
         String descricao,
+        @NotNull
         BigDecimal valor,
+        @NotNull
         LocalDateTime data,
         Boolean ativo) {
 }
--- src/main/java/financeiro/api/service/ReceitaService.java
+++ src/main/java/financeiro/api/service/ReceitaService.java
@@ -16,12 +16,16 @@ public class ReceitaService {
     @Autowired
     private ReceitaRepository receitaRepository;
     public Receita cadastrar(CadastrarReceitaDto dto) {
+        verificarReceitaComMesmaDescricaoNoMesmoMes(dto);
+        return receitaRepository.save(new Receita(dto));
+    }
+
+    private void verificarReceitaComMesmaDescricaoNoMesmoMes(CadastrarReceitaDto dto) {
         Optional<Receita> receitaComMesmaDescricao = receitaRepository.getReferenceByDescricao(dto.descricao());
         if(!receitaComMesmaDescricao.isEmpty()){
             if(receitaComMesmaDescricao.get().getData().getMonthValue() == dto.data().getMonthValue()){
-                throw new ValidacaoException("Encontramos uma receita com essa mesma descriçãore registrada no banco de dados!");
+                throw new ValidacaoException("A API não deve permitir o cadastro de receitas duplicadas(contendo a mesma descrição, dentro do mesmo mês");
             }
         }
-        return receitaRepository.save(new Receita(dto));
     }
 }
