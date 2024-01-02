51fc3d27bbf1c3c846fcfb42dd713619ab506d20
Klayton Souza <klayton.al.souza@gmail.com>
2024-01-02 09:52:55 -0300

    1.15 - Codigo de teste da classe: ReceitaService

--- src/test/java/financeiro/api/service/ReceitaServiceTest.java
+++ src/test/java/financeiro/api/service/ReceitaServiceTest.java
@@ -1,9 +1,13 @@
 package financeiro.api.service;
 
+import financeiro.api.dto.receita.AtualizacaoParcialReceitaDto;
+import financeiro.api.dto.receita.AtualizacaoTotalReceitaDto;
 import financeiro.api.dto.receita.ReceitaDto;
+import financeiro.api.dto.receita.ReceitaMesDto;
 import financeiro.api.exception.ValidacaoException;
 import financeiro.api.model.receita.Receita;
 import financeiro.api.repository.ReceitaRepository;
+import org.hamcrest.collection.IsEmptyCollection;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -144,4 +148,135 @@ class ReceitaServiceTest {
         assertEquals(resultadoFindAllByAtivoTrue.size(), resultadoListar.size());
 
     }
+    @Test
+    @DisplayName("Testando detalhamento de uma receita passando um ID que esta cadastrado no banco de dados!")
+    void detalhar01(){
+        Long id_receita = 1L;
+        Receita receita = new Receita(1L,"Teste POST",new BigDecimal(600.00), LocalDateTime.now(),true);
+        when(receitaRepository.existsById(id_receita)).thenReturn(true);
+        when(receitaRepository.getReferenceById(id_receita)).thenReturn(receita);
+
+        receitaService.detalhar(id_receita);
+
+
+        verify(receitaRepository,times(1)).getReferenceById(id_receita);
+    }
+    @Test
+    @DisplayName("Testando detalhamento de uma receita passando um ID que não esta cadastrado no banco de dados!")
+    void detalhar02(){
+
+        Long id_receita = 1L;
+        Receita receita = new Receita(1L,"Teste POST",new BigDecimal(600.00), LocalDateTime.now(),true);
+        when(receitaRepository.existsById(id_receita)).thenReturn(false);
+
+        assertThrows(ValidacaoException.class, () -> receitaService.detalhar(id_receita));
+
+       verify(receitaRepository,times(1)).existsById(id_receita);
+    }
+
+    @Test
+    @DisplayName("Testando se a atualização parcial da receita esta alterando os valor (Descrição) corretamente")
+    void atualizacaoParcial01(){
+        AtualizacaoParcialReceitaDto dto = new AtualizacaoParcialReceitaDto(
+                1L,
+                "parcial",
+                null,
+                null);
+        Receita receita = new Receita(
+                1L,
+                "teste",
+                new BigDecimal(1000.00)
+                ,LocalDateTime.of(2024,Month.JANUARY,1,10,10,0)
+                ,true);
+
+        when(receitaRepository.existsById(dto.id_receita())).thenReturn(true);
+        when(receitaRepository.getReferenceById(dto.id_receita())).thenReturn(receita);
+
+        receitaService.parcial(dto);
+
+        assertEquals(receita.getDescricao(),dto.descricao());
+        assertNotEquals(receita.getValor(),dto.valor());
+    }
+    @Test
+    @DisplayName("Testando se a atualização parcial da receita esta alterando os valor (Descrição) corretamente")
+    void atualizaçãoTotal(){
+        AtualizacaoTotalReceitaDto dto = new AtualizacaoTotalReceitaDto(
+                1L,
+                "total",
+                new BigDecimal(700.00),
+                LocalDateTime.now());
+        Receita receita = new Receita(
+                1L,
+                "parcial",
+                new BigDecimal(1000.00)
+                ,LocalDateTime.of(2024,Month.JANUARY,1,10,10,0)
+                ,true);
+
+        when(receitaRepository.existsById(dto.id_receita())).thenReturn(true);
+        when(receitaRepository.getReferenceById(dto.id_receita())).thenReturn(receita);
+
+        receitaService.total(dto);
+
+        assertEquals(receita.getDescricao(),dto.descricao());
+        assertEquals(receita.getValor(),dto.valor());
+        assertEquals(receita.getData(),dto.data());
+
+    }
+
+    @Test
+    @DisplayName("Verificando se a exclução logica foi executado corretamente, o parametro (ativo) deve ser alterado")
+    void deletar(){
+        Long id_receita = 1L;
+        Receita receita = new Receita(
+                1L,
+                "parcial",
+                new BigDecimal(1000.00)
+                ,LocalDateTime.of(2024,Month.JANUARY,1,10,10,0)
+                ,true);
+        when(receitaRepository.existsById(id_receita)).thenReturn(true);
+        when(receitaRepository.getReferenceById(id_receita)).thenReturn(receita);
+
+        receitaService.exclusaoLogica(id_receita);
+        assertEquals(receita.getAtivo(),false);
+    }
+
+    @Test
+    @DisplayName("Executando um select na tabela de receitas com mes e ano especifico!")
+    void listarMes01(){
+        int ano = 2024;
+        int mes = 1;
+
+        ReceitaMesDto receita01 = new ReceitaMesDto(
+                "Potter",
+                new BigDecimal(50.00),
+                LocalDateTime.now()
+        );
+        ReceitaMesDto receita02 = new ReceitaMesDto(
+                "Potter",
+                new BigDecimal(50.00),
+                LocalDateTime.of(2024,1,1,10,0)
+        );
+        List<ReceitaMesDto> receitaDtoList = Arrays.asList(receita01,receita02);
+        when(receitaRepository.listarPorMesEAno(ano,mes)).thenReturn(receitaDtoList);
+
+        receitaService.listarMes(ano,mes);
+
+        verify(receitaRepository,times(1)).listarPorMesEAno(ano,mes);
+        assertEquals(receitaDtoList.size(),2);
+    }
+    @Test
+    @DisplayName("Retornar uma exception pelo fato de que na data especifica não foi encontrado nenhum registro")
+    void listarMes02(){
+        int ano = 2024;
+        int mes = 1;
+        when(receitaRepository.listarPorMesEAno(ano,mes)).thenReturn(new ArrayList<>());
+
+        ValidacaoException exception = assertThrows(ValidacaoException.class, () -> receitaService.listarMes(ano, mes));
+
+        assertEquals("Não foi possivel encontrar nenhuma receita no banco de dados nessa data!",exception.getMessage());
+        verify(receitaRepository, times(1)).listarPorMesEAno(ano, mes);
+    }
+
+
+
 }
